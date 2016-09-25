8d4d789f7e1f4e50975c20225e312695324c8368
Malek Mammar <malek.mammar@gmail.com>
2016-09-25 14:15:15 +0200

    adapting port listening

--- index.js
+++ index.js
@@ -1,23 +1,66 @@
 'use strict';
 
 var ical = require('ical-generator'),
-    http = require('http'),
+    app = require('express')(),
+    request = require('request'),
+    logger = console,
     cal = ical({
-        domain: 'sebbo.net',
-        prodId: '//superman-industries.com//ical-generator//EN',
-        events: [
-            {
-                start: new Date(),
-                end: new Date(new Date().getTime() + 3600000),
-                summary: 'Example Event',
-                description: 'It works ;)',
-                url: 'http://sebbo.net/'
-            }
-        ]
+        domain: 'iae-grenoble.fr',
+        prodId: '//iae-grenoble.fr//icalendar//EN',
     });
 
-http.createServer(function(req, res) {
-    cal.serve(res);
-}).listen(3000, '127.0.0.1', function() {
-    console.log('Server running at http://127.0.0.1:3000/');
+// authenticating
+var authenticate = function (credentials, cb) {
+    var form = {
+        username: credentials.username || "",
+        password: credentials.password || "",
+        intranet_annee_precedente:"0",
+        Connexion:"Connexion"
+    }
+    request.post({url:'http://intranet.iae-grenoble.fr/index/index', form: form}, cb)    
+}
+
+// getting events
+var getEvents = function (req, res, cookie) {
+    request.get({url:'http://intranet.iae-grenoble.fr/full-calendar/evenements?planningType=0&start=1474236000&end=1487977200',
+                headers:{'Cookie':cookie}}, function (err, httpResponse, body) {
+        if (err) {
+            logger.error('Failed to authenticate : ', err);
+            return res.status(400).json({status: 400, result: null, error: "Failed to authenticate"});
+        }
+        
+        try {
+            body = JSON.parse(body);
+        } catch (e) {
+            logger.error('Failed to authenticate');
+            return res.status(400).json({status: 400, result: null, error: "Failed to authenticate"});
+        }
+        
+        var events = []
+        for(var event in body)
+        {
+            events.push({
+                start: body[event].start,
+                end: body[event].end,
+                summary: body[event].title,
+                url: body[event].url
+            });
+        }
+        //logger.log(events);
+        cal.events(events);
+        cal.serve(res);
+    })
+}
+
+
+var port = process.env.PORT || 5000;
+app.use(function(req, res) {
+    authenticate(req.query || {}, function (err, httpResponse, body) {
+        //logger.log(httpResponse.headers['set-cookie']);
+        getEvents(req, res, httpResponse.headers['set-cookie'][0]);
+    });
+});
+
+app.listen(port, function() {
+    logger.log('Server running at http://127.0.0.1:'+port);
 });
